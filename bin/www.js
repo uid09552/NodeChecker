6bf25832186bef4186103c4d6b639622d36f23ca
uid09552 <max.rgbg@gmail.com>
2015-07-15 21:28:12 +0200

    jen

--- bin/www.js
+++ bin/www.js
@@ -72,6 +72,8 @@ app.get('/SayHello', function (req, res) {
 
     function isAuthenticated(req, res, next) {
 
+
+
         // check header or url parameters or post parameters for token
         var token = req.body.token || req.query.token || req.headers['x-auth-token'];
 
