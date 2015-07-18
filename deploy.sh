24b446f50240c5032bd7e4fe19f0a0d6640143af
max.hoch@hotmail.com <max.hoch@hotmail.com>
2015-07-18 13:45:17 +0200

    Please enter the commit message for your changes. Lines starting

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 npm install
 export PORT=3001
-#forever stopall
-#forever start app.js
+forever stopall
+forever start app.js
 echo 'forever started'
-#forever list
-node app.js
-curl localhost:3001/start
+forever list
+
+
