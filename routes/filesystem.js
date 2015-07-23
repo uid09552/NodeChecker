0b08ec91a0d7faafec893d0fa1d07b6eb56c69f5
uid09552 <max.rgbg@gmail.com>
2015-07-23 22:50:24 +0200

    filesystem check removed

--- routes/filesystem.js
+++ routes/filesystem.js
@@ -3,8 +3,7 @@
  */
 var express=require('express');
 var router=express.Router();
-var discspace = require('diskspace');
-var njds = require('nodejs-disks');
+
 var basicHC = require('basic-healthcheck');
 
 router.get('/',function(req,res){
@@ -14,38 +13,6 @@ router.get('/',function(req,res){
 });
 
 
-router.get('/filesystem',function(req,res){
-    var alldiscstatus = [];
-    var discstatus;
-    var status;
-    njds.drives(
-        function (err, drives) {
-            njds.drivesDetail(
-                drives,
-                function (err, data) {
-                    if (err) {
-                        console.error(err);
-                    }
-                    for (var i = 0; i < data.length; i++) {
-
-                        discstatus = {
-                            "mountpoint": data[i].mountpoint, "Total": data[i].total, "Used": data[i].used,
-                            "available": data[i].available,
-                            "Name": data[i].drive, "FreePerc": data[i].freePer
-                        };
-                        alldiscstatus.push(discstatus);
-                    }
-                    console.log(alldiscstatus);
-                    res.status(201);
-                    res.send(alldiscstatus);
-                }
-            );
-        }
-    );
-
-
-
-});
 
 
 module.exports=router;
